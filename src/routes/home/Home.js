7a2216a0d1191c1f51f02e14c0348f968af45bca
Manh Pham <manhhailua@gmail.com>
2016-09-09 09:36:13 +0700

    html fixes

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -58,16 +58,13 @@ class Home extends Component {
             <div className="small-box bg-aqua">
               <div className="inner">
                 <h3>150</h3>
-                <p>
-                  New Orders
-                </p>
+                <p>New Orders</p>
               </div>
               <div className="icon">
                 <i className="ion ion-bag" />
               </div>
               <a href="/empty" className="small-box-footer">
-                More info
-                <i className="fa fa-arrow-circle-right" />
+                More info <i className="fa fa-arrow-circle-right" />
               </a>
             </div>
           </div>
@@ -78,16 +75,13 @@ class Home extends Component {
               <div className="inner">
                 <h3>53<sup style={{ fontSize: 20 }}>%</sup>
                 </h3>
-                <p>
-                  Bounce Rate
-                </p>
+                <p>Bounce Rate</p>
               </div>
               <div className="icon">
                 <i className="ion ion-stats-bars" />
               </div>
               <a href="/empty" className="small-box-footer">
-                More info
-                <i className="fa fa-arrow-circle-right" />
+                More info <i className="fa fa-arrow-circle-right" />
               </a>
             </div>
           </div>
@@ -97,16 +91,13 @@ class Home extends Component {
             <div className="small-box bg-yellow">
               <div className="inner">
                 <h3>44</h3>
-                <p>
-                  User Registrations
-                </p>
+                <p>User Registrations</p>
               </div>
               <div className="icon">
                 <i className="ion ion-person-add" />
               </div>
               <a href="/empty" className="small-box-footer">
-                More info
-                <i className="fa fa-arrow-circle-right" />
+                More info <i className="fa fa-arrow-circle-right" />
               </a>
             </div>
           </div>
@@ -116,16 +107,13 @@ class Home extends Component {
             <div className="small-box bg-red">
               <div className="inner">
                 <h3>65</h3>
-                <p>
-                  Unique Visitors
-                </p>
+                <p>Unique Visitors</p>
               </div>
               <div className="icon">
                 <i className="ion ion-pie-graph" />
               </div>
               <a href="/empty" className="small-box-footer">
-                More info
-                <i className="fa fa-arrow-circle-right" />
+                More info <i className="fa fa-arrow-circle-right" />
               </a>
             </div>
           </div>
